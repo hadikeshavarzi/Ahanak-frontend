bb24fd716fb51252172de5d0d7109d407454990d
hadikeshavarzi <hadi.keshavarzi@gmail.com>
2025-11-22 06:13:24 +0000

    set clinet config

--- src/sanity/config/client-config.ts
+++ src/sanity/config/client-config.ts
@@ -1,12 +1,12 @@
-import { type ClientConfig } from 'next-sanity';
+import { type ClientConfig } from "next-sanity";
 
-const config = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: 'production',
-  apiVersion: '2023-03-09',
+const config: ClientConfig = {
+  projectId: process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2023-03-09",
   useCdn: true,
-  token: process.env.SANITY_PROJECT_API_TOKEN!,
-  perspective: 'published',
-} satisfies ClientConfig;
+  token: process.env.SANITY_PROJECT_API_TOKEN, // لازم نیست ! بگذاری
+  perspective: "published",
+};
 
 export default config;
