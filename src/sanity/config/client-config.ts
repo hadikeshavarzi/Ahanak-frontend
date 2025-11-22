13004f88c2cff1c7efdca815d3b323ed345bc653
hadikeshavarzi <hadi.keshavarzi@gmail.com>
2025-11-22 06:33:13 +0000

    Fix sanity env config

--- src/sanity/config/client-config.ts
+++ src/sanity/config/client-config.ts
@@ -1,12 +1,13 @@
-import { type ClientConfig } from "next-sanity";
-
-const config: ClientConfig = {
-  projectId: process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2023-03-09",
-  useCdn: true,
-  token: process.env.SANITY_PROJECT_API_TOKEN, // لازم نیست ! بگذاری
+import { type ClientConfig } from "next-sanity"; 
+const projectId =
+  process.env.SANITY_PROJECT_ID || 
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+const dataset = process.env.SANITY_DATASET || 
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 
+  "production";
+const config: ClientConfig = { projectId, dataset, 
+  apiVersion: "2023-03-09", useCdn: true, token: 
+  process.env.SANITY_PROJECT_API_TOKEN, 
   perspective: "published",
 };
-
 export default config;
