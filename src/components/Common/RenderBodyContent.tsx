f4a81213ed4c81620a83ffbc24e7a2e2c49c3767
hadikeshavarzi <hadi.keshavarzi@gmail.com>
2025-11-22 09:22:25 +0000

    fix sanity image builder

--- src/components/Common/RenderBodyContent.tsx
+++ src/components/Common/RenderBodyContent.tsx
@@ -1,43 +1,43 @@
-import config from "@/sanity/config/client-config";
+import { client } from "@/sanity/sanity-utils";
 import { Blog } from "@/types/blogItem";
 import { PortableText } from "@portabletext/react";
 import { getImageDimensions } from "@sanity/asset-utils";
 import urlBuilder from "@sanity/image-url";
 import Image from "next/image";
 
-// Barebones lazy-loaded image component
+// Lazy Image Component
 const SampleImageComponent = ({ value, isInline }: any) => {
-  const { width, height } = getImageDimensions(value);
-  return (
-    <Image
-      src={urlBuilder(config).image(value).fit("max").auto("format").url()!}
-      width={width}
-      height={height}
-      alt={value.alt || "blog image"}
-      loading="lazy"
-      style={{
-        // Display alongside text if image appears inside a block text span
-        display: isInline ? "inline-block" : "block",
+    const { width, height } = getImageDimensions(value);
 
-        // Avoid jumping around with aspect-ratio CSS property
-        aspectRatio: width / height,
-      }}
-    />
-  );
+    const imageUrl = urlBuilder(client)
+        .image(value)
+        .fit("max")
+        .auto("format")
+        .url();
+
+    return (
+        <Image
+            src={imageUrl!}
+            width={width}
+            height={height}
+            alt={value.alt || "blog image"}
+            loading="lazy"
+            style={{
+                display: isInline ? "inline-block" : "block",
+                aspectRatio: width / height,
+            }}
+        />
+    );
 };
 
 const components = {
-  types: {
-    image: SampleImageComponent,
-  },
+    types: {
+        image: SampleImageComponent,
+    },
 };
 
 const RenderBodyContent = ({ post }: { post: Blog }) => {
-  return (
-    <>
-      <PortableText value={post?.body || []} components={components} />
-    </>
-  );
+    return <PortableText value={post?.body || []} components={components} />;
 };
 
 export default RenderBodyContent;
